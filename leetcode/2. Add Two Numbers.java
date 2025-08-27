416c1350c90b5577900305af1bc1cecc03ad0eef
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-08-27 22:15:50 +0530

    Update 2. Add Two Numbers.java

--- leetcode/2. Add Two Numbers.java
+++ leetcode/2. Add Two Numbers.java
@@ -1,5 +1,5 @@
 //2. Add Two Numbers
-//works for 1564/1569 test cases, the issue is that test case 1565 introduces a linked list with a number too long to be stored in a long.
+//runtime beats only 11.31%, memory usage beats 40.67%
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -12,33 +12,49 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int l1num = 0, l2num = 0;
-        String l1text = "", l2text = "";
+        ArrayList<Integer> l1num = new ArrayList<>();
+        ArrayList<Integer> l2num = new ArrayList<>();
+        ArrayList<Integer> sumOfNums = new ArrayList<>();
         ListNode l1Track = l1, l2Track = l2;
         while(l1Track!=null) {
-            l1text += l1Track.val;
+            l1num.add(l1Track.val);
             l1Track = l1Track.next;
         }
         while(l2Track!=null) {
-            l2text += l2Track.val;
+            l2num.add(l2Track.val);
             l2Track = l2Track.next;
         }
-        StringBuilder res1 = new StringBuilder();
-        StringBuilder res2 = new StringBuilder();
-        String resl1 = res1.append(l1text).reverse().toString();
-        String resl2 = res2.append(l2text).reverse().toString();
-        long sum = Long.parseLong(resl1) + Long.parseLong(resl2);
-        String result = "";
-        result += sum;
-        ListNode listSum = new ListNode();
-        ListNode listSumTrack = listSum;
-        for (int i = result.length()-1; i >= 0; i--) {
-            listSumTrack.val = result.charAt(i) - '0';
-            if(i!=0) {
-                listSumTrack.next = new ListNode();
-                listSumTrack = listSumTrack.next;
+        int trackLarge = l1num.size() > l2num.size() ? l1num.size() : l2num.size();
+        ListNode sum = new ListNode();
+        ListNode sumTrack = sum;
+        for (int i = 0; i < trackLarge; i++) {
+            if((i < l1num.size()) && (i < l2num.size())) {
+                sumOfNums.add(l1num.get(i)+l2num.get(i));
             }
+            else if(i < l1num.size())
+                sumOfNums.add(l1num.get(i));
+            else if(i < l2num.size())
+                sumOfNums.add(l2num.get(i));
         }
-        return listSum;
+        for (int i = 0; i < trackLarge-1; i++) {
+            if(sumOfNums.get(i)>9) {
+                sumOfNums.set(i, sumOfNums.get(i)-10);
+                sumOfNums.set(i+1, sumOfNums.get(i+1)+1);
+            }
+            sumTrack.val = sumOfNums.get(i);
+            sumTrack.next = new ListNode();
+            sumTrack = sumTrack.next;
+        }
+        if(sumOfNums.get(trackLarge-1)>9) {
+            sumOfNums.set(trackLarge-1, sumOfNums.get(trackLarge-1)-10);
+            sumOfNums.add(1);
+        }
+        sumTrack.val = sumOfNums.get(trackLarge-1);
+        if(sumOfNums.size()>trackLarge) {
+            sumTrack.next = new ListNode();
+            sumTrack = sumTrack.next;
+            sumTrack.val = 1;
+        }
+        return sum;
     }
 }
