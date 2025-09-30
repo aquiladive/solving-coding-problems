9900864178c5eff4f2138c8e04c5578084cb41de
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-09-30 23:58:05 +0530

    Update 1935. Maximum Number of Words You Can Type.java

--- leetcode/1935. Maximum Number of Words You Can Type.java
+++ leetcode/1935. Maximum Number of Words You Can Type.java
@@ -1,5 +1,5 @@
 //1935. Maximum Number of Words You Can Type
-//beats 25.91% of solutions in runtime, beats 9.26% of solutions in memory
+//beats 89.67% of solutions in runtime, beats 74.30% of solutions in memory
 
 class Solution {
     public int canBeTypedWords(String text, String brokenLetters) {
@@ -8,10 +8,7 @@ class Solution {
         for(int i = 0; i < words.length; i++) {
             String check = words[i];
             for(int j = 0; j < check.length(); j++) {
-                char[] character = new char[1];
-                character[0] = check.charAt(j);
-                String checkChar = new String(character);
-                if(brokenLetters.contains(checkChar))
+                if(brokenLetters.indexOf(check.charAt(j)) != -1)
                     break;
                 if(j == check.length() - 1)
                     wordCount++;
