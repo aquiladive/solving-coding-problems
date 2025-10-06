b84a60deeb851733ade8007c2960ba1590685dbc
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-10-06 22:40:00 +0530

    Update 58. Length of Last Word.java

--- leetcode/58. Length of Last Word.java
+++ leetcode/58. Length of Last Word.java
@@ -7,3 +7,20 @@ class Solution {
         return words[words.length - 1].length();
     }
 }
+
+//second solution
+//less elegant, more efficient
+//runtime beats 100%, memory beats 91.60%
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int wordLength = 0;
+        for(int i = s.length()-1; i >= 0; i--) {
+            if(s.charAt(i) != ' ')
+                wordLength++;
+            else if(wordLength != 0)
+                break;
+        }
+        return wordLength;
+    }
+}
