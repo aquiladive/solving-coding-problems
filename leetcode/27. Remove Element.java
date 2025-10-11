b42b7f3aaea198733851b634b31131adcc494cbd
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-10-11 08:42:30 +0530

    Update 27. Remove Element.java

--- leetcode/27. Remove Element.java
+++ leetcode/27. Remove Element.java
@@ -17,3 +17,32 @@ class Solution {
         return count;
     }
 }
+
+//second solution, using the hints
+//runtime beats 100%, memory beats 17.72%
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int dupCount = 0, i = 0, j = nums.length-1;
+        if(j == 0) {
+            if(nums[0] == val)
+                dupCount++;
+        }
+        while(i <= j && i < nums.length && j > 0) {
+            while(nums[j] == val && j > 0) {
+                nums[j] = 0;
+                j--;
+                dupCount++;
+            }
+            if(j < 0)
+                break;
+            if(nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+                dupCount++;
+            }
+            i++;
+        }
+        return nums.length - dupCount;
+    }
+}
