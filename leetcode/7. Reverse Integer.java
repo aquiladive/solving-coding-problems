910f51abc7209dfd690d8b8ae1e3946fa009b11b
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-10-13 23:10:25 +0530

    Update 7. Reverse Integer.java

--- leetcode/7. Reverse Integer.java
+++ leetcode/7. Reverse Integer.java
@@ -43,3 +43,39 @@ class Solution {
         }
     }
 }
+
+//third solution
+//runtime beats 81.28%, memory only beats 5.27%
+
+class Solution {
+    public int reverse(int x) {
+        boolean negative = false;
+        int count = 0;
+        if(x < 0) {
+            negative = true;
+            x *= -1;
+        }
+        int copy = x;
+        int reverse = 0;
+        while(x > 0) {
+            reverse = (reverse * 10) + x % 10;
+            x = x/10;
+            count++;
+        }
+        if(reverse < 0)
+            return 0;
+        if(count == 10) {
+            int reverseCopy = reverse;
+            for(int i = count-1; i > 0; i--) {
+                if((copy % 10) != (int)(reverseCopy/(Math.pow(10, i))))
+                    return 0;
+                copy = copy/10;
+                reverseCopy = reverseCopy % (int)(Math.pow(10, i));
+            }
+        }
+
+        if(negative)
+            reverse *= -1;
+        return reverse;
+    }
+}
