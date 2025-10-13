5e86bedd874f75aab269a34b0dc7ed5fb3e9c1ad
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-10-13 20:39:39 +0530

    Update 7. Reverse Integer.java

--- leetcode/7. Reverse Integer.java
+++ leetcode/7. Reverse Integer.java
@@ -21,3 +21,25 @@ class Solution {
         }
     }
 }
+
+//second solution
+//more elegent, slightly more efficient
+//runtime beats only 3.09%, memory beats only 34.56%
+
+class Solution {
+    public int reverse(int x) {
+        String s = "";
+        if(x < 0) {
+            x *= -1;
+            s += "-";
+        }
+        s += new StringBuilder(Integer.toString(x)).reverse().toString();
+        try {
+            int reverse = Integer.parseInt(s);
+            return reverse;
+        }
+        catch(NumberFormatException e) {
+            return 0;
+        }
+    }
+}
