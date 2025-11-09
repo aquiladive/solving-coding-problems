d556b2e9b6698e74d81257d101bfbc7d452e7c8d
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-11-09 23:53:17 +0530

    Update 283. Move Zeroes.java

--- leetcode/283. Move Zeroes.java
+++ leetcode/283. Move Zeroes.java
@@ -15,3 +15,21 @@ class Solution {
         }
     }
 }
+
+//another way
+//runtime beats 80.10%, memory beats only 6.95%
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0, j = 0;
+        while(j < nums.length) {
+            if(nums[j] != 0) {
+                nums[i++] = nums[j++];
+            }
+            else
+                j++;
+        }
+        while(i < nums.length)
+            nums[i++] = 0;
+    }
+}
