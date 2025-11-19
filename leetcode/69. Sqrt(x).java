e95bd769c138caef5f37dfecd142fb3ea88b3ce8
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2025-11-19 23:42:40 +0530

    Update 69. Sqrt(x).java

--- leetcode/69. Sqrt(x).java
+++ leetcode/69. Sqrt(x).java
@@ -1,14 +1,18 @@
 //69. Sqrt(x)
-//incomplete, naive solution
+//runtime beats only 5.14%, memory beats only 8.53%
 
 class Solution {
     public int mySqrt(int x) {
+        if(x < 2)
+            return x;
         long multiplied = 0;
-        for(int i = x/2; i > 0; i--) {
-            multiplied = (long)i*i;
-            if(multiplied <= x)
-                return i;
+        int count = 2;
+        while(count <= x/2) {
+            multiplied = (long)count*count;
+            if(multiplied > x)
+                return count - 1;
+            count++;
         }
-        return x;
+        return count - 1;
     }
 }
